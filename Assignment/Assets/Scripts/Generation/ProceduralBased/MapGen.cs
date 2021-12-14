644104dfe335cd84247c716cb0b6d543b16ab042
adam25022 <C18363931@mytudublin.ie>
2021-12-14 19:48:01 +0000

    refactoring code and commenting

---
+++ Assignment/Assets/Scripts/Generation/ProceduralBased/MapGen.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGen : MonoBehaviour {
+
+	public enum DrawMode {PerlinNoise, WorldMap, Mesh, FalloffMap};
+	public DrawMode drawMode;
+	public Noise.NormalizeMode normalizeMode;
+	
+	public int SizeOfMap;
+
+	public float noiseScale;
+	public int Octaves;
+	[Range(0,1)]
+	public float Persistance;
+	public float Lacunarity;
+	public int Seed;
+	public Vector2 Offset;
+	public float MeshHeightChanger;
+	public AnimationCurve MeshHeightCurveAmmount;
+	public bool autoUpdate;
+	public TerrainType[] regions;
+	float[,] falloffMap;
+	void Awake() {
+		falloffMap = FallOffMap.GenerateFalloffMap (SizeOfMap);
+	}
+
+	public void DrawMapInEditor() {
+		MapData mapData = GenerateMapData (Vector2.zero);
+
+		AllDisplays display = FindObjectOfType<AllDisplays> ();
+		if (drawMode == DrawMode.PerlinNoise) {
+			display.CreateMapTexture (TextureGen.TextureFromHeightMap (mapData.heightMap));
+		} else if (drawMode == DrawMode.WorldMap) {
+			display.CreateMapTexture (TextureGen.TextureFromWorldMap (mapData.WorldMap, SizeOfMap, SizeOfMap));
+		} else if (drawMode == DrawMode.Mesh) {
+			display.CreateMesh (MeshGen.GenerateTerrainMesh (mapData.heightMap, MeshHeightChanger, MeshHeightCurveAmmount), TextureGen.TextureFromWorldMap (mapData.WorldMap, SizeOfMap, SizeOfMap));
+		} else if (drawMode == DrawMode.FalloffMap) {
+			display.CreateMapTexture(TextureGen.TextureFromHeightMap(FallOffMap.GenerateFalloffMap(SizeOfMap)));
+		}
+	}
+
+	MapData GenerateMapData(Vector2 centre) {
+		// this retrieves the noisemap from the noise.cs file using the inputted data of size, seed, noise, octaves, persistance, lacrunarity, offset and the way it is being normalized.
+		float[,] PerlinNoise = Noise.GenerateNoiseMap (SizeOfMap, SizeOfMap, Seed, noiseScale, Octaves, 
+														Persistance, Lacunarity, centre + Offset, normalizeMode);
+		//generate the colours for all of the pixels. 
+		//this is a 1d array as opposed to the perlin noise which is 2d.
+		Color[] WorldMap = new Color[SizeOfMap * SizeOfMap];
+		//again its a 2d array so this is the y co-ordinate
+		for (int y = 0; y < SizeOfMap; y++) {
+			//this is the x co-ordinate
+			for (int x = 0; x < SizeOfMap; x++) {
+				// here we tell it to set all the pieces on the outside of the block to use the falloff map
+				PerlinNoise [x, y] = Mathf.Clamp01(PerlinNoise [x, y] - falloffMap [x, y]);
+				float CurrentHeight = PerlinNoise [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (CurrentHeight >= regions [i].Height) {
+						//here since its a 1d array to get the index by multiplying Y by the width of the map
+						// as it will give us the index of the row we are on, then add the x value(pixelnum on the x)
+						WorldMap [y * SizeOfMap + x] = regions [i].Color;
+					} else {
+						break;
+					}
+				}
+			}
+		}
+
+
+		return new MapData (PerlinNoise, WorldMap);
+	}
+
+	void OnValidate() {
+		if (Lacunarity < 1) {
+			Lacunarity = 1;
+		}
+		if (Octaves < 0) {
+			Octaves = 0;
+		}
+
+		falloffMap = FallOffMap.GenerateFalloffMap (SizeOfMap);
+	}
+	void Start(){
+		DrawMapInEditor ();
+	}
+		
+}
+
+[System.Serializable]
+public struct TerrainType {
+	public string Name;
+	public float Height;
+	public Color Color;
+}
+
+public struct MapData {
+	public readonly float[,] heightMap;
+	public readonly Color[] WorldMap;
+
+	public MapData (float[,] heightMap, Color[] WorldMap)
+	{
+		this.heightMap = heightMap;
+		this.WorldMap = WorldMap;
+	}
+}
---
+++ Assignment/Assets/Scripts/Generation/ProceduralBased/TextureGen.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public static class TextureGen {
+
+	public static Texture2D TextureFromWorldMap(Color[] WorldMap, int width, int Height) {
+		Texture2D texture = new Texture2D (width, Height);
+		texture.filterMode = FilterMode.Point;
+		texture.wrapMode = TextureWrapMode.Clamp;
+		texture.SetPixels (WorldMap);
+		texture.Apply ();
+		return texture;
+	}
+
+
+	public static Texture2D TextureFromHeightMap(float[,] heightMap) {
+		int width = heightMap.GetLength (0);
+		int Height = heightMap.GetLength (1);
+
+		Color[] WorldMap = new Color[width * Height];
+		for (int y = 0; y < Height; y++) {
+			for (int x = 0; x < width; x++) {
+				WorldMap [y * width + x] = Color.Lerp (Color.black, Color.white, heightMap [x, y]);
+			}
+		}
+
+		return TextureFromWorldMap (WorldMap, width, Height);
+	}
+
+}
