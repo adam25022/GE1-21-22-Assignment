0d5947262a7af68ad35432b5f0babdecafbb4548
adam25022 <C18363931@mytudublin.ie>
2021-12-14 01:51:31 +0000

    implemented a ring of cubes interacting with the audio

--- Assignment/Assets/Scripts/Generation/AudioPeer.cs
+++ Assignment/Assets/Scripts/Generation/AudioPeer.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class AudioPeer : MonoBehaviour
 {   
     AudioSource _AudioSource;
-    public float[] _samples = new float[512];
+    public static float[] _samples = new float[512];
     // Start is called before the first frame update
     void Start()
     {
