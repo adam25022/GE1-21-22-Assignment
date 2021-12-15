e869dfe99cb6cce172031409496498858b21bf83
adam25022 <C18363931@mytudublin.ie>
2021-12-15 04:49:23 +0000

    attempt to raycast from click

--- Assignment/Assets/Scripts/Generation/ProceduralBased/Trees/SpawnTreeFromRaycast.cs
+++ Assignment/Assets/Scripts/Generation/ProceduralBased/Trees/SpawnTreeFromRaycast.cs
@@ -9,10 +9,21 @@ public class SpawnTreeFromRaycast : MonoBehaviour
     {
         
     }
+    Ray myRay; // create ray
+    RaycastHit hit;//creat the raycast hit
+
+    public GameObject Tree;
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRay=Camera.main.ScreenPointToRay(Input.mousePosition);//comes from the camera to mouse.
+        if(Physics.Raycast(myRay, out hit))
+        {
+            if(Input.GetMouseButtonDown(0))//if you click.
+            {
+                Instantiate(Tree, hit.point, Quaternion.identity);//this creates a tree on click.
+            }
+        }
     }
 }
